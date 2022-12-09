b5ab346212e35841e3bdb922b8ac2fbec2060747
bobbykim89 <bobby.sihun.kim@gmail.com>
2022-12-09 02:05:27 -0700

    initial commit

---
+++ pages/About.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci
+      corrupti a asperiores incidunt perferendis nesciunt enim quis fugit neque
+      officiis!
+    </p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci
+      corrupti a asperiores incidunt perferendis nesciunt enim quis fugit neque
+      officiis!
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
